72990d8106a277878b864ebc075d3fe60f1c65e4
Tony Knudson <tonyknudson91@gmail.com>
2025-11-05 20:22:10 -0600

    added support for dynamic input

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import sys
 from stats import get_book_text
 
 def main():
-    path_to_file = "./books/frankenstein.txt"
-    get_book_text(path_to_file)
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_file = sys.argv[1]
+        get_book_text(path_to_file)
+    print(sys.argv)
+    # Prints ['main.py']
 
 main()
